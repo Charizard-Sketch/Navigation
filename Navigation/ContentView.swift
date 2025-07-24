d5759cba7c2523ac041fcb64dc71a1c3e732e663
Montserrat <jaureguimontse16@gmail.com>
2025-07-24 10:12:11 -0700

    I add a new vstack navigation stack  navigation link and i added new text

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("This is the root view")
+                NavigationLink(destination: Text("You've arrived at the second view!")) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                }
+                
+            }
+
+            
+            
+            
         }
-        .padding()
     }
 }
 
