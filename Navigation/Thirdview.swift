08a819687a94ce59a4d92dfeba770ce391d6a79c
Montserrat <jaureguimontse16@gmail.com>
2025-07-24 15:25:10 -0700

    i added a third view

---
+++ Navigation/Thirdview.swift
@@ -0,0 +1,18 @@
+//
+//  Thirdview.swift
+//  Navigation
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+struct Thirdview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Thirdview()
+}
